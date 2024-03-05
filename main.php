5211f70fa0b846c69330cbe59854c477bb0f38f0
Everton da Rosa <everton3x@gmail.com>
2024-03-05 14:04:27 -0300

    Finalização do main.php

--- main.php
+++ main.php
@@ -1,7 +1,20 @@
 <?php
 
 // para teste
-$remessa = 202401;
+//$remessa = 202401;
+
+
+// Pega dados do usuário
+echo "Conversão dos TXT do PAD.", PHP_EOL;
+echo "Digite os dados solicitados.", PHP_EOL;
+
+echo "Ano [AAAA]: ";
+fscanf(STDIN, "%d\n", $ano);
+echo "Mês [MM] (1 ~ 13): ";
+fscanf(STDIN, "%d\n", $mes);
+$mes = str_pad($mes, 2, '0', STR_PAD_LEFT);
+
+$remessa = (int) $ano.$mes;
 
 // Configurações
 $msc_output_base_dir = 'Z:\\MSC\\';
@@ -16,7 +29,7 @@ echo 'Conectado ao banco de dados'.PHP_EOL;
 $output_file_path = parse_remessa_to_filepath($msc_output_base_dir, $remessa);
 echo "Carregando dados de $output_file_path".PHP_EOL;
 $data = load_source_csv($output_file_path);
-printf('Linhas carregadas: %d', sizeof($data));
+printf('Linhas carregadas: %d'.PHP_EOL, sizeof($data));
 
 
 echo 'Convertendo linhas...'.PHP_EOL;
